483f6c07da7c8e4f32deab9f5e49984cb90de720
Seunfunmib <bamidelesheun@gmail.com>
2023-06-23 22:44:51 +0100

    progress

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,7 +15,17 @@ module.exports = {
         "desaturated-red":"#ce9797",
         "soft-red":"#f96262",
         "dark-grayish-red":"#413a3a",
-      }
+      },
+      letterSpacing: {
+        tightest: '-.075em',
+        tighter: '-.05em',
+        tight: '-.025em',
+        normal: '0',
+        wide: '.025em',
+        wider: '.05em',
+        widest: '.1em',
+        widest: '.25em',
+      },
     },
   },
   plugins: [],
